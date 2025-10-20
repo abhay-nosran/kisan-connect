c9d8ac14685712d77f88e808bba29ca9b3683720
Abhay Nosran <abhaynosran@outlook.com>
2025-10-20 13:33:54 +0530

    Enabled CORS

--- kisanConnectBackend-2/broadCastServer/register.js
+++ kisanConnectBackend-2/broadCastServer/register.js
@@ -1,11 +1,11 @@
 const express = require("express") ;
 const subscribeController = require("./SubscribeController")
 const messageReader = require("./messageReader")
-
+const cors = require("cors")
 const app = new express() ;
 const PORT = 3333
 
-
+app.use(cors()) ;
 messageReader() ;
 
 app.get("/subscribe",subscribeController) ;
